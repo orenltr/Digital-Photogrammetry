335d331cb0a4ec8e8b4b6b4e1a6be552cd29b147
oren lauterman <orenltr@gmail.com>
2020-11-24 19:05:26 +0200

    accuracy_rate parameter number_of_samples parameter

--- Hw1/hw1.py
+++ Hw1/hw1.py
@@ -9,20 +9,20 @@ from tkinter.filedialog import askopenfilenames
 # #%%
 
 
-def sampleGroundTruth(img):
+def sampleGroundTruth(img, number_of_samples):
 
     # sampling object and background pixels
 
-    plt.title('sample 50 points of the object', fontweight="bold")
+    plt.title('sample ',number_of_samples/2, ' points of the object', fontweight="bold")
     plt.imshow(img2)
-    object_points = plt.ginput(50)
+    object_points = plt.ginput(number_of_samples/2)
     object_points = np.array(object_points, dtype=int)
     print(object_points)
     plt.close()
 
-    plt.title('sample 50 points of the background', fontweight="bold")
+    plt.title('sample ',number_of_samples/2, 'points of the background', fontweight="bold")
     plt.imshow(img2)
-    background_points = plt.ginput(50)
+    background_points = plt.ginput(number_of_samples/2)
     background_points = np.array(background_points, dtype=int)
     print(background_points)
 
@@ -40,9 +40,9 @@ def sampleGroundTruth(img):
 def generateGaborFiltersBank():
 
     kernels = []  # holds the kernels we will generate
-    for theta in range(4):  # define number of thetas (orientation of the filter)
-        theta = theta / 4 * np.pi
-        for sigma in (1, 3):  # define number of sigmas (std of the gaussian envelope)
+    for theta in range(8):  # define number of thetas (orientation of the filter)
+        theta = theta / 8 * np.pi
+        for sigma in (1, 3, 5, 7):  # define number of sigmas (std of the gaussian envelope)
             for lamda in np.arange(np.pi / 4, np.pi, np.pi / 4):  # define number of wavelengths
                 for gamma in (0.05, 0.5):  # define number of gammas
                     ksize = 9
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 
     # load image
     # img_path = askopenfilenames(title='Select Input File')
-    img_name = '20201121_143330' # 20201121_143330, 20201121_143257, 20201121_143402
+    img_name = '20201121_143402'  # 20201121_143330, 20201121_143257, 20201121_143402
     img = cv2.imread(img_name+'.jpg')
     img = cv2.resize(img, (200, 300))  # resize image to ease calculations
 
@@ -71,50 +71,53 @@ if __name__ == '__main__':
     # convert to gray scale
     img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# #%%
-#     # create data frame in which each column is a filter and each row is a pixel
-#     df = pd.DataFrame()
-#
-#     # save original image as a vector and insert to df
-#     vec_img2 = img2.reshape(-1)
-#     df['Original Image'] = vec_img2
-#
-#     # sample ground truth of the object and the background
-#     # use to determine if a filter contribute to the segmentation
-#     # use also the evaluate the accuracy of the resulting segmentation
-#     sampled_pixels = sampleGroundTruth(img2)
-#
-#     # generate filter bank
-#     kernels = generateGaborFiltersBank()
-#
-#     classified_correctly = 0  # keeps the number of correctly classified pixels (from the sampled pixels)
-#
-#     # generate Gabor features
-#     for i, kernel in enumerate(kernels):
-#
-#         gabor_label = 'Gabor' + str(i)  # label of column in the data frame
-#         # filter the image
-#         filtered_image = cv2.filter2D(img2, cv2.CV_8UC3, kernel)
-#         # add the filtered image as vector to data frame
-#         vec_filtered_img2 = filtered_image.reshape(-1)
-#         df[gabor_label] = vec_filtered_img2
-#
-#         classified_correctly_new = correctlyClassified(df, sampled_pixels)
-#         if classified_correctly_new > classified_correctly:
-#             classified_correctly = classified_correctly_new
-#             # save the filtered img
-#             cv2.imwrite('gabor_filtered_images/' + img_name + '/' + gabor_label + '.jpg', filtered_image)
-#
-#         else:
-#             df.drop(columns=gabor_label, inplace=True)
-#
-#         # plt.imshow(filtered_image)
-#         # plt.show()
-#
-#
-#
-#     df.head()
-#     df.to_csv(img_name+'.csv', index=False)
+#%%
+    # create data frame in which each column is a filter and each row is a pixel
+    df = pd.DataFrame()
+
+    # save original image as a vector and insert to df
+    vec_img2 = img2.reshape(-1)
+    df['Original Image'] = vec_img2
+
+    # sample ground truth of the object and the background
+    # use to determine if a filter contribute to the segmentation
+    # use also the evaluate the accuracy of the resulting segmentation
+    number_of_samples = 100
+    sampled_pixels = sampleGroundTruth(img2,number_of_samples)
+
+    # generate filter bank
+    kernels = generateGaborFiltersBank()
+
+    classified_correctly = 0  # keeps the number of correctly classified pixels (from the sampled pixels)
+
+    # generate Gabor features
+    for i, kernel in enumerate(kernels):
+
+        gabor_label = 'Gabor' + str(i)  # label of column in the data frame
+        # filter the image
+        filtered_image = cv2.filter2D(img2, cv2.CV_8UC3, kernel)
+        # add the filtered image as vector to data frame
+        vec_filtered_img2 = filtered_image.reshape(-1)
+        df[gabor_label] = vec_filtered_img2
+
+        classified_correctly_new = correctlyClassified(df, sampled_pixels)
+        if classified_correctly_new > classified_correctly:
+            classified_correctly = classified_correctly_new
+            accuracy_rate = classified_correctly/number_of_samples *100
+            print(accuracy_rate, ' % of pixels been classified correctly')
+            # save the filtered img
+            cv2.imwrite('gabor_filtered_images/' + img_name + '/' + gabor_label + '.jpg', filtered_image)
+
+        else:
+            df.drop(columns=gabor_label, inplace=True)
+
+        # plt.imshow(filtered_image)
+        # plt.show()
+
+
+
+    df.head()
+    df.to_csv(img_name+'.csv', index=False)
 #%%
 
     df = pd.read_csv(img_name+'.csv')
@@ -149,4 +152,4 @@ if __name__ == '__main__':
     plt.show()
 
     # cv2.imwrite('segmented_results/'+img_name+'.jpg', segmented_img)
-    fig.savefig('segmented_results/'+img_name+'_kernel_5.jpg')
+    fig.savefig('segmented_results/'+img_name+'_kernel_9.jpg')
